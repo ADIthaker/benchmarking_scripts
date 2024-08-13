13b90b13858927406d13c83714abfde7c0bb0a53
root <root@node-0.adit2801-213683.escra-pg0.wisc.cloudlab.us>
2024-08-12 19:33:57 -0500

    made exp setup

--- start_exp.sh
+++ start_exp.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-numactl --cpunodebind=$1 --membind=$1 iperf3 -c 10.10.1.1 -B 10.10.1.2
+
+echo "Running Reset CPUS"
+./reset_cpus.sh
+
+echo "Running Set CPUS"
+./set_cpus.sh $1 2
+
+numactl --cpunodebind=$1 --membind=$1 iperf3 -c 10.10.1.1 -B 10.10.1.2 > exp$1.log
